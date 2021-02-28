36f3e6e588b6d48c4274e7d4409afc097e13b662
junyu <fly12610@gmail.com>
2021-02-28 22:42:00 +0800

    first todoList sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoList';
+  todos = [];
+  todoItem: string;
+  insertNewTodo() {
+    if (this.todoItem && !this.todos.some(items => items === this.todoItem)) {
+      this.todos.push(this.todoItem);
+
+    } else { alert('please check new todo is not exist or not empty'); }
+
+    this.todoItem = '';
+  }
+  delete(idx) {
+    this.todos.splice(idx, 1);
+  }
 }
